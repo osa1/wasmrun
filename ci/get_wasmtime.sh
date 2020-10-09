bba24bce2dff466abb6f1e0669c21e640b8e1848
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-10-09 05:44:55 +0300

    ci: Download wasmtime in WASI test job

---
+++ ci/get_wasmtime.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/bytecodealliance/wasmtime/releases/download/v0.20.0/wasmtime-v0.20.0-x86_64-linux.tar.xz
+tar -xvf wasmtime-v0.20.0-x86_64-linux.tar.xz
+cp wasmtime-v0.20.0-x86_64-linux/wasmtime .
