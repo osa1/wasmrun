1b7590118e62e939dd61e63010954f23897db8b2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2022-09-30 22:43:32 +0200

    SIMD: More instructions

--- crates/libwasmrun/src/exec/simd.rs
+++ crates/libwasmrun/src/exec/simd.rs
@@ -1,5 +1,6 @@
 use crate::exec::{trapping_add, ExecError, MemIdx, Result};
 use crate::store::ModuleAddr;
+use crate::value::{canonicalize_f32_nan, canonicalize_f64_nan};
 use crate::Runtime;
 
 use libwasmrun_syntax::{MemArg, SimdInstruction};
@@ -243,16 +244,6 @@ pub fn exec_simd_instr(
             rt.stack.push_i32(vec[usize::from(lane_idx)] as i32)?;
         }
 
-        SimdInstruction::I8x16Eq => {
-            let v2 = rt.stack.pop_i128()?.to_le_bytes();
-            let v1 = rt.stack.pop_i128()?.to_le_bytes();
-            let mut res: [u8; 16] = [0; 16];
-            for i in 0..16 {
-                res[i] = if v1[i] == v2[i] { 0xFF } else { 0x00 };
-            }
-            rt.stack.push_i128(i128::from_le_bytes(res))?;
-        }
-
         SimdInstruction::V128Not => {
             let mut v = rt.stack.pop_i128()?.to_le_bytes();
             for i in 0..16 {
@@ -304,9 +295,51 @@ pub fn exec_simd_instr(
             rt.stack.push_i128(i128::from_le_bytes(res))?;
         }
 
-        SimdInstruction::F32x4Mul => f32x4_lanewise_zip_map(rt, |f1, f2| f1 * f2)?,
+        SimdInstruction::I8x16Neg => i8x16_lanewise_map(rt, |i| ((i as i8).wrapping_neg()) as u8)?,
+
+        SimdInstruction::I16x8Neg => i16x8_lanewise_map(rt, |i| -i)?,
+
+        SimdInstruction::F32x4Mul => {
+            f32x4_lanewise_zip_map(rt, |f1, f2| canonicalize_f32_nan(f1 * f2))?
+        }
+
+        SimdInstruction::F32x4Div => {
+            f32x4_lanewise_zip_map(rt, |f1, f2| canonicalize_f32_nan(f1 / f2))?
+        }
+
+        SimdInstruction::F32x4Neg => f32x4_lanewise_map(rt, |f| -f)?,
+
+        SimdInstruction::F32x4Sqrt => f32x4_lanewise_map(rt, |f| canonicalize_f32_nan(f.sqrt()))?,
+
+        SimdInstruction::F32x4Add => {
+            f32x4_lanewise_zip_map(rt, |f1, f2| canonicalize_f32_nan(f1 + f2))?
+        }
+
+        SimdInstruction::F32x4Sub => {
+            f32x4_lanewise_zip_map(rt, |f1, f2| canonicalize_f32_nan(f1 - f2))?
+        }
 
         SimdInstruction::F32x4Abs => f32x4_lanewise_map(rt, f32::abs)?,
+
+        SimdInstruction::F64x2Mul => {
+            f64x2_lanewise_zip_map(rt, |f1, f2| canonicalize_f64_nan(f1 * f2))?
+        }
+
+        SimdInstruction::F64x2Div => {
+            f64x2_lanewise_zip_map(rt, |f1, f2| canonicalize_f64_nan(f1 / f2))?
+        }
+
+        SimdInstruction::F64x2Neg => f64x2_lanewise_map(rt, |f| -f)?,
+
+        SimdInstruction::F64x2Sqrt => f64x2_lanewise_map(rt, |f| canonicalize_f64_nan(f.sqrt()))?,
+
+        SimdInstruction::F64x2Add => {
+            f64x2_lanewise_zip_map(rt, |f1, f2| canonicalize_f64_nan(f1 + f2))?
+        }
+
+        SimdInstruction::F64x2Sub => {
+            f64x2_lanewise_zip_map(rt, |f1, f2| canonicalize_f64_nan(f1 - f2))?
+        }
 
         SimdInstruction::F64x2Abs => f64x2_lanewise_map(rt, f64::abs)?,
 
@@ -533,12 +566,119 @@ pub fn exec_simd_instr(
             rt.stack.push_i32(i)?;
         }
 
+        SimdInstruction::F32x4ExtractLane(lane) => {
+            let v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            let i = f32::from_le_bytes(v[lane * 4..lane * 4 + 4].try_into().unwrap());
+            rt.stack.push_f32(i)?;
+        }
+
         SimdInstruction::I64x2ExtractLane(lane) => {
             let v = rt.stack.pop_i128()?.to_le_bytes();
             let lane = usize::from(lane);
             let i = i64::from_le_bytes(v[lane * 8..lane * 8 + 8].try_into().unwrap());
             rt.stack.push_i64(i)?;
         }
+
+        SimdInstruction::F64x2ExtractLane(lane) => {
+            let v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            let i = f64::from_le_bytes(v[lane * 8..lane * 8 + 8].try_into().unwrap());
+            rt.stack.push_f64(i)?;
+        }
+
+        SimdInstruction::I8x16ReplaceLane(lane) => {
+            let i = rt.stack.pop_i32()? as u8;
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            v[usize::from(lane)] = i;
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::I16x8ReplaceLane(lane) => {
+            let i = (rt.stack.pop_i32()? as u16).to_le_bytes();
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            v[lane * 2 + 0] = i[0];
+            v[lane * 2 + 1] = i[1];
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::I32x4ReplaceLane(lane) => {
+            let i = rt.stack.pop_i32()?.to_le_bytes();
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            v[lane * 4 + 0] = i[0];
+            v[lane * 4 + 1] = i[1];
+            v[lane * 4 + 2] = i[2];
+            v[lane * 4 + 3] = i[3];
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::F32x4ReplaceLane(lane) => {
+            let i = rt.stack.pop_f32()?.to_le_bytes();
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            v[lane * 4 + 0] = i[0];
+            v[lane * 4 + 1] = i[1];
+            v[lane * 4 + 2] = i[2];
+            v[lane * 4 + 3] = i[3];
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::I64x2ReplaceLane(lane) => {
+            let i = rt.stack.pop_i64()?.to_le_bytes();
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            v[lane * 8 + 0] = i[0];
+            v[lane * 8 + 1] = i[1];
+            v[lane * 8 + 2] = i[2];
+            v[lane * 8 + 3] = i[3];
+            v[lane * 8 + 4] = i[4];
+            v[lane * 8 + 5] = i[5];
+            v[lane * 8 + 6] = i[6];
+            v[lane * 8 + 7] = i[7];
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::F64x2ReplaceLane(lane) => {
+            let i = rt.stack.pop_f64()?.to_le_bytes();
+            let mut v = rt.stack.pop_i128()?.to_le_bytes();
+            let lane = usize::from(lane);
+            v[lane * 8 + 0] = i[0];
+            v[lane * 8 + 1] = i[1];
+            v[lane * 8 + 2] = i[2];
+            v[lane * 8 + 3] = i[3];
+            v[lane * 8 + 4] = i[4];
+            v[lane * 8 + 5] = i[5];
+            v[lane * 8 + 6] = i[6];
+            v[lane * 8 + 7] = i[7];
+            rt.stack.push_i128(i128::from_le_bytes(v))?;
+        }
+
+        SimdInstruction::V128AnyTrue => {
+            let v = rt.stack.pop_i128()?;
+            rt.stack.push_i32(if v == 0 { 0 } else { 1 })?;
+        }
+
+        SimdInstruction::I8x16Eq => i8x16_rel(rt, |i1, i2| i1 == i2)?,
+
+        SimdInstruction::I8x16Ne => i8x16_rel(rt, |i1, i2| i1 != i2)?,
+
+        SimdInstruction::I8x16LtS => i8x16_rel(rt, |i1, i2| (i1 as i8) < (i2 as i8))?,
+
+        SimdInstruction::I8x16LtU => i8x16_rel(rt, |i1, i2| i1 < i2)?,
+
+        SimdInstruction::I8x16LeS => i8x16_rel(rt, |i1, i2| (i1 as i8) <= (i2 as i8))?,
+
+        SimdInstruction::I8x16LeU => i8x16_rel(rt, |i1, i2| i1 <= i2)?,
+
+        SimdInstruction::I8x16GtS => i8x16_rel(rt, |i1, i2| (i1 as i8) > (i2 as i8))?,
+
+        SimdInstruction::I8x16GtU => i8x16_rel(rt, |i1, i2| i1 > i2)?,
+
+        SimdInstruction::I8x16GeS => i8x16_rel(rt, |i1, i2| (i1 as i8) >= (i2 as i8))?,
+
+        SimdInstruction::I8x16GeU => i8x16_rel(rt, |i1, i2| i1 >= i2)?,
 
         SimdInstruction::I32x4Eq => i32x4_rel(rt, |i1, i2| i1 == i2)?,
 
@@ -559,6 +699,18 @@ pub fn exec_simd_instr(
         SimdInstruction::I32x4GeS => i32x4_rel(rt, |i1, i2| i1 >= i2)?,
 
         SimdInstruction::I32x4GeU => i32x4_rel(rt, |i1, i2| (i1 as u32) >= (i2 as u32))?,
+
+        SimdInstruction::F32x4Eq => f32x4_rel(rt, |i1, i2| i1 == i2)?,
+
+        SimdInstruction::F32x4Ne => f32x4_rel(rt, |i1, i2| i1 != i2)?,
+
+        SimdInstruction::F32x4Lt => f32x4_rel(rt, |i1, i2| i1 < i2)?,
+
+        SimdInstruction::F32x4Le => f32x4_rel(rt, |i1, i2| i1 <= i2)?,
+
+        SimdInstruction::F32x4Gt => f32x4_rel(rt, |i1, i2| i1 > i2)?,
+
+        SimdInstruction::F32x4Ge => f32x4_rel(rt, |i1, i2| i1 >= i2)?,
 
         SimdInstruction::I64x2Eq => i64x2_rel(rt, |i1, i2| i1 == i2)?,
 
@@ -675,6 +827,36 @@ pub fn exec_simd_instr(
     Ok(())
 }
 
+fn i8x16_rel<F>(rt: &mut Runtime, rel: F) -> Result<()>
+where
+    F: Fn(u8, u8) -> bool,
+{
+    let v2 = rt.stack.pop_i128()?.to_le_bytes();
+    let v1 = rt.stack.pop_i128()?.to_le_bytes();
+    let mut ret = [0u8; 16];
+    for i in 0..16 {
+        ret[i] = if rel(v1[i], v2[i]) { 0xFF } else { 0 };
+    }
+    rt.stack.push_i128(i128::from_le_bytes(ret))
+}
+
+fn i16x2_rel<F>(rt: &mut Runtime, rel: F) -> Result<()>
+where
+    F: Fn(i16, i16) -> bool,
+{
+    let v2 = vec_to_i16x8(rt.stack.pop_i128()?);
+    let v1 = vec_to_i16x8(rt.stack.pop_i128()?);
+    let mut ret = [0i16; 8];
+    for i in 0..8 {
+        ret[i] = if rel(v1[i], v2[i]) {
+            0xFFFFu16 as i16
+        } else {
+            0i16
+        };
+    }
+    rt.stack.push_i128(i16x8_to_vec(ret))
+}
+
 fn i32x4_rel<F>(rt: &mut Runtime, rel: F) -> Result<()>
 where
     F: Fn(i32, i32) -> bool,
@@ -690,6 +872,23 @@ where
         };
     }
     rt.stack.push_i128(i32x4_to_vec(ret))
+}
+
+fn f32x4_rel<F>(rt: &mut Runtime, rel: F) -> Result<()>
+where
+    F: Fn(f32, f32) -> bool,
+{
+    let v2 = vec_to_f32x4(rt.stack.pop_i128()?);
+    let v1 = vec_to_f32x4(rt.stack.pop_i128()?);
+    let mut ret = [0f32; 4];
+    for i in 0..4 {
+        ret[i] = if rel(v1[i], v2[i]) {
+            f32::from_le_bytes([0xFF; 4])
+        } else {
+            0f32
+        };
+    }
+    rt.stack.push_i128(f32x4_to_vec(ret))
 }
 
 fn i64x2_rel<F>(rt: &mut Runtime, rel: F) -> Result<()>
@@ -724,6 +923,28 @@ where
         };
     }
     rt.stack.push_i128(f64x2_to_vec(ret))
+}
+
+fn i8x16_lanewise_map<F>(rt: &mut Runtime, f: F) -> Result<()>
+where
+    F: Fn(u8) -> u8,
+{
+    let mut v = rt.stack.pop_i128()?.to_le_bytes();
+    for i in 0..16 {
+        v[i] = f(v[i]);
+    }
+    rt.stack.push_i128(i128::from_le_bytes(v))
+}
+
+fn i16x8_lanewise_map<F>(rt: &mut Runtime, f: F) -> Result<()>
+where
+    F: Fn(i16) -> i16,
+{
+    let mut v = vec_to_i16x8(rt.stack.pop_i128()?);
+    for i in 0..8 {
+        v[i] = f(v[i]);
+    }
+    rt.stack.push_i128(i16x8_to_vec(v))
 }
 
 fn f32x4_lanewise_zip_map<F>(rt: &mut Runtime, f: F) -> Result<()>
@@ -774,6 +995,20 @@ where
     rt.stack.push_i128(f64x2_to_vec(v))
 }
 
+fn vec_to_i16x8(v: i128) -> [i16; 8] {
+    let bytes = v.to_le_bytes();
+    [
+        i16::from_le_bytes([bytes[0], bytes[1]]),
+        i16::from_le_bytes([bytes[2], bytes[3]]),
+        i16::from_le_bytes([bytes[4], bytes[5]]),
+        i16::from_le_bytes([bytes[6], bytes[7]]),
+        i16::from_le_bytes([bytes[8], bytes[9]]),
+        i16::from_le_bytes([bytes[10], bytes[11]]),
+        i16::from_le_bytes([bytes[12], bytes[13]]),
+        i16::from_le_bytes([bytes[14], bytes[15]]),
+    ]
+}
+
 fn vec_to_f32x4(v: i128) -> [f32; 4] {
     let bytes = v.to_le_bytes();
     [
@@ -816,6 +1051,36 @@ fn vec_to_i64x2(v: i128) -> [i64; 2] {
             bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
         ]),
     ]
+}
+
+fn i16x8_to_vec(is: [i16; 8]) -> i128 {
+    let [i1, i2, i3, i4, i5, i6, i7, i8] = is;
+    let i1_bytes = i1.to_le_bytes();
+    let i2_bytes = i2.to_le_bytes();
+    let i3_bytes = i3.to_le_bytes();
+    let i4_bytes = i4.to_le_bytes();
+    let i5_bytes = i5.to_le_bytes();
+    let i6_bytes = i6.to_le_bytes();
+    let i7_bytes = i7.to_le_bytes();
+    let i8_bytes = i8.to_le_bytes();
+    i128::from_le_bytes([
+        i1_bytes[0],
+        i1_bytes[1],
+        i2_bytes[0],
+        i2_bytes[1],
+        i3_bytes[0],
+        i3_bytes[1],
+        i4_bytes[0],
+        i4_bytes[1],
+        i5_bytes[0],
+        i5_bytes[1],
+        i6_bytes[0],
+        i6_bytes[1],
+        i7_bytes[0],
+        i7_bytes[1],
+        i8_bytes[0],
+        i8_bytes[1],
+    ])
 }
 
 fn f32x4_to_vec(fs: [f32; 4]) -> i128 {
