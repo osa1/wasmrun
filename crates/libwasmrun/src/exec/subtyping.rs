d497fb0374a328c4813fc0eed1a4c9c7c1f52cce
Ömer Sinan Ağacan <omeragacan@gmail.com>
2024-03-19 10:54:53 +0100

    GC: More subtyping

--- crates/libwasmrun/src/exec/subtyping.rs
+++ crates/libwasmrun/src/exec/subtyping.rs
@@ -1,11 +1,13 @@
+use crate::module::{Module, TypeIdx};
+
 use libwasmrun_syntax::{CompType, HeapType, ValueType};
 
 #[allow(unused)]
-pub(super) fn is_subtype_of(
+pub(super) fn is_value_subtype_of(
     sub_ty: &ValueType,
     super_ty: &ValueType,
-    sub_ty_module_tys: &[CompType],
-    super_ty_module_tys: &[CompType],
+    sub_ty_module: &Module,
+    super_ty_module: &Module,
 ) -> bool {
     let sub_ref_ty = match sub_ty {
         ValueType::I32 | ValueType::I64 | ValueType::F32 | ValueType::F64 | ValueType::V128 => {
@@ -28,16 +30,17 @@ pub(super) fn is_subtype_of(
     is_heap_subtype_of(
         &sub_ref_ty.heap_ty,
         &super_ref_ty.heap_ty,
-        sub_ty_module_tys,
-        super_ty_module_tys,
+        sub_ty_module,
+        super_ty_module,
     )
 }
 
-fn is_heap_subtype_of(
+#[allow(unused)]
+pub(crate) fn is_heap_subtype_of(
     sub_ty: &HeapType,
     super_ty: &HeapType,
-    sub_ty_module_tys: &[CompType],
-    _super_ty_module_tys: &[CompType],
+    sub_ty_module: &Module,
+    super_ty_module: &Module,
 ) -> bool {
     match super_ty {
         // Bottom types are only subtypes of the bottom types of same kind.
@@ -66,16 +69,20 @@ fn is_heap_subtype_of(
         // TODO: Shouldn't externs be `eq` as well?
         HeapType::Eq => match sub_ty {
             HeapType::Array | HeapType::Struct | HeapType::I31 => true,
-            HeapType::TypeIdx(sub_ty_idx) => {
-                !matches!(sub_ty_module_tys[*sub_ty_idx as usize], CompType::Func(_))
-            }
+            HeapType::TypeIdx(sub_ty_idx) => !matches!(
+                sub_ty_module.get_type(TypeIdx(*sub_ty_idx)),
+                CompType::Func(_)
+            ),
             _ => false,
         },
 
         HeapType::Func => match sub_ty {
             HeapType::Func => true,
             HeapType::TypeIdx(sub_ty_idx) => {
-                matches!(sub_ty_module_tys[*sub_ty_idx as usize], CompType::Func(_))
+                matches!(
+                    sub_ty_module.get_type(TypeIdx(*sub_ty_idx)),
+                    CompType::Func(_)
+                )
             }
             _ => false,
         },
@@ -83,7 +90,10 @@ fn is_heap_subtype_of(
         HeapType::Struct => match sub_ty {
             HeapType::Func => true,
             HeapType::TypeIdx(sub_ty_idx) => {
-                matches!(sub_ty_module_tys[*sub_ty_idx as usize], CompType::Struct(_))
+                matches!(
+                    sub_ty_module.get_type(TypeIdx(*sub_ty_idx)),
+                    CompType::Struct(_)
+                )
             }
             _ => false,
         },
@@ -91,11 +101,60 @@ fn is_heap_subtype_of(
         HeapType::Array => match sub_ty {
             HeapType::Func => true,
             HeapType::TypeIdx(sub_ty_idx) => {
-                matches!(sub_ty_module_tys[*sub_ty_idx as usize], CompType::Array(_))
+                matches!(
+                    sub_ty_module.get_type(TypeIdx(*sub_ty_idx)),
+                    CompType::Array(_)
+                )
             }
             _ => false,
         },
 
-        HeapType::TypeIdx(_super_ty_idx) => todo!(),
+        HeapType::TypeIdx(super_ty_idx) => {
+            // Super type is a struct, array, or function type.
+            match sub_ty {
+                HeapType::Any => false,
+
+                HeapType::None => true,
+
+                HeapType::Exn => false,
+
+                HeapType::Extern => false,
+
+                HeapType::NoExtern => false,
+
+                HeapType::Func => false,
+
+                HeapType::NoFunc => matches!(
+                    super_ty_module.get_type(TypeIdx(*super_ty_idx)),
+                    CompType::Func(_)
+                ),
+
+                HeapType::Eq => false,
+
+                HeapType::Struct => matches!(
+                    super_ty_module.get_type(TypeIdx(*super_ty_idx)),
+                    CompType::Struct(_)
+                ),
+
+                HeapType::Array => matches!(
+                    super_ty_module.get_type(TypeIdx(*super_ty_idx)),
+                    CompType::Array(_)
+                ),
+
+                HeapType::I31 => false,
+
+                HeapType::TypeIdx(sub_ty_idx) => {
+                    let super_canonical_ty_idx =
+                        super_ty_module.canonical_type_ids[*super_ty_idx as usize];
+
+                    let sub_canonical_ty_idx =
+                        sub_ty_module.canonical_type_ids[*sub_ty_idx as usize];
+
+                    // TODO: We need to map canonical type indices to super types' canonical
+                    // indices, and compare super types of the subtype with the supertype.
+                    super_canonical_ty_idx == sub_canonical_ty_idx
+                }
+            }
+        }
     }
 }
